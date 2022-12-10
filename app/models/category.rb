42e5c1d753fc17133682533a683b76f312d40de1
kyuuki <kyuuki.japan@gmail.com>
2022-12-10 03:30:15 +0000

    i18n 化

---
+++ app/models/category.rb
@@ -0,0 +1,9 @@
+class Category < ActiveHash::Base
+  self.data = [
+    { id: 1, name_i18n: "category.novel" },
+    { id: 2, name_i18n: "category.humanity" },
+    { id: 3, name_i18n: "category.science" },
+    { id: 4, name_i18n: "category.it" },
+    { id: 99999, name_i18n: "category.other" },
+  ]
+end
